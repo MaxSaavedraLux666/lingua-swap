808414d32f9de6438e9cea5035a8901c860a4218
Max Saavedra <max.saavedra@unmsm.edu.pe>
2024-05-14 15:24:41 -0500

    Implementacion

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 """app.py"""
 from flask import Flask, render_template, url_for
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 
-from routes.login import login_bp
-from routes.home import home_bp
+# from routes.login import login_bp
+# from routes.home import home_bp
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+# bootstrap = Bootstrap(app)
 # Registra el blueprint para las rutas de login
-app.register_blueprint(login_bp)
-app.register_blueprint(home_bp)
+# app.register_blueprint(login_bp)
+# app.register_blueprint(home_bp)
 
 
 @app.route('/')
@@ -17,8 +17,21 @@ def index():
     """
         Pagina de inicio
     """
-    return render_template('home.html')
+    return render_template('index.html')
 
+@app.route('/topics-listing')
+def topics_listing():
+    """
+    Página de listado de temas
+    """
+    return render_template('topics-listing.html')
+
+@app.route('/contact')
+def contact():
+    """
+    Página de listado de temas
+    """
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/routes/login.py
+++ app/routes/login.py
@@ -1,32 +1,27 @@
-from flask import Flask, render_template
-from login import login_bp
+from flask import Blueprint, render_template, redirect, url_for
 
-app = Flask(__name__)
+# app = Flask(__name__)
 # app.register_blueprint(login_bp, url_prefix='/auth')  # Prefijo para las rutas de autenticación
 # Prefijo para las rutas de autenticación
-app.register_blueprint(login_bp, url_prefix='/auth')
+login_bp = Blueprint('login', __name__)
 
 # Ruta para la página principal
 
 
-@app.route('/')
+@login_bp.route('/')
 def home():
     return render_template('home.html')
 
 # Ruta para la página de registro
 
 
-@app.route('/registro')
+@login_bp.route('/registro')
 def register():
     return render_template('register.html')
 
 # Ruta para la página de registro exitoso
 
 
-@app.route('/registro_exitoso')
+@login_bp.route('/registro_exitoso')
 def registration_success():
     return render_template('registro_exitoso.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
